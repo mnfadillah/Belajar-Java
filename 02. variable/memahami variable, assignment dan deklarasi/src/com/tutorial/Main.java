9e58faac283c76ac60df14f4887cf73b1b699716
mnfadillah <53703057+mnfadillah@users.noreply.github.com>
2019-08-08 12:40:53 +0700

    Add files via upload

---
+++ 02. variable/memahami variable, assignment dan deklarasi/src/com/tutorial/Main.java
@@ -0,0 +1,22 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args){
+
+        // kita akan membuat variable
+        // tipe data
+        int x = 10; //assignment
+        System.out.println("nilai a = " + x);
+
+        x = 20;
+        System.out.println("nilai x baru = " + x);
+
+        // kita akan membuat
+        int b; // deklarasi
+
+        b = 7;
+
+        System.out.println("nilai b = " + b);
+
+    }
+}
